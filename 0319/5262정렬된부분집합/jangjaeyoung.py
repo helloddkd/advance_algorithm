b05eb8ed0b5be3a8efc35cd891f2bb8fc3da8e4c
JY <snowink1137@gmail.com>
2019-03-19 17:57:36 +0900

    0319 jangjaeyoung

---
+++ 0319/5262정렬된부분집합/jangjaeyoung.py
@@ -0,0 +1,23 @@
+import sys
+
+sys.stdin = open('5262.txt', 'r')
+
+
+T = int(input())
+for test_case in range(1, T+1):
+    information = list(map(int, input().split()))
+
+    N = information[0]
+    numbers = information[1:]
+
+    D = [1 for _ in range(information[0])]
+
+    for i in range(1, N):
+        for j in range(i):
+            if numbers[j] < numbers[i]:
+                if D[i] <= D[j]:
+                    D[i] = D[j] + 1
+
+    result = max(D)
+
+    print('#{} {}'.format(test_case, result))
